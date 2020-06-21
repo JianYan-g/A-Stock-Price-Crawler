244de21f8f7cc09ad62823f3029a93880c8a1f84
sdtech-yangjian <49139754+sdtech-yangjian@users.noreply.github.com>
2020-06-22 07:26:30 +0900

    Csharp

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace GetData
+{
+    partial class 事前課題１
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 204);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(817, 440);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(183, 44);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(247, 22);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(183, 95);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(247, 22);
+            this.dateTimePicker2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(509, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Get Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "From:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Until";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(577, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(183, 145);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // 事前課題１
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 656);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "事前課題１";
+            this.Text = "事前課題1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace GetData
+{
+    public partial class 事前課題１ : Form
+    {
+        DataTable table=new DataTable();
+        List<DataRow> DataList=new List<DataRow>();
+        public 事前課題１()
+        {
+            InitializeComponent();
+        }
+        public void InitTable()
+        {
+            table = new DataTable("MarketDataTable");
+            table.Columns.Add("日付", typeof(string));
+            table.Columns.Add("終値", typeof(string));
+            dataGridView1.DataSource = table;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {        
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //  Datetime dt1=dateTimePicker1.Value.Year.ToString("yyyy/MM/dd");
+           
+            String StartDate = "";
+            String EndDate = "";
+            string time = "1965.1.5 1:0:0";
+            DateTime bottom = new DateTime();
+            bottom = DateTime.Parse(time);
+
+            StartDate=(-157453200 + 24*60*60*Convert.ToInt32(dateTimePicker1.Value.Subtract(bottom).TotalDays)).ToString();
+            EndDate= (-157453200 + 24*60*60*Convert.ToInt32(dateTimePicker2.Value.Subtract(bottom).TotalDays)).ToString();
+
+            InitTable();
+
+            string result = "";
+            HtmlWeb web = new HtmlWeb();//*
+            var doc = web.Load("https://finance.yahoo.com/quote/%5EN225/history?period1="+StartDate+"&period2="+EndDate+"&interval=1d&filter=history&frequency=1d");
+            var Date_Nodes = doc.DocumentNode.SelectNodes("//section/div[2]//table/tbody//tr//td[1]");          
+            var Close_Nodes = doc.DocumentNode.SelectNodes("//section/div[2]//table/tbody//tr//td[5]");
+
+            if (Date_Nodes == null)
+            {
+                label3.Text = "NO DATA AVAILABLE";
+            }
+            else if (Date_Nodes != null)
+            {
+                
+                foreach (HtmlNode item in Date_Nodes)
+                {
+                    DataRow dr = table.NewRow();
+                    dr[1] = "";
+                    dr[0] = item.InnerText;
+                    DataList.Add(dr);
+                    /*result = item.InnerText;
+                    // table.Rows.Add(new Object[] { 1, "Smith" });
+                    DataRow dr = table.NewRow();
+                    dr[0] = result;
+                    
+                    table.Rows.Add(dr);*/
+                }
+                int i = 0;
+                foreach (HtmlNode item in Close_Nodes)
+                {
+                    DataRow dr2 = DataList[i];
+                    dr2[1] = item.InnerText;
+                    DataList[i] = dr2;
+                    i++;
+                }
+                DataList.Reverse();
+
+                foreach (DataRow dr in DataList)
+                {
+                    table.Rows.Add(dr);
+                }
+                dataGridView1.DataSource = table;
+                dataGridView1.AllowUserToAddRows = false;
+            }
+            else
+            {
+                // Every. Single. Time.
+            }
+            table = new DataTable();
+            DataList = new List<DataRow>();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+            
+    
